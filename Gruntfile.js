a5ce76f950504dcbcbc8aefbda265e129e03db9c
Paul <herppfel@gmail.com>
2015-03-28 02:38:46 -0400

    grunt updates
    js + css minification... still working on livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,8 +18,34 @@ module.exports = function(grunt) {
 
     watch: {
       options: {
-        livereload: true
-      }
+        livereload: true,
+      },
+
+
+      js: {
+        files: [ 'js/*.js', 'js/**/*.js' ],
+        tasks: [ 'concat', 'uglify' ],
+        options: {
+          spawn: false,
+        },
+        /*livereload: {
+          livereload: true,
+          files: [ 'js/global.min.js' ],
+        }*/
+      },
+
+      css: {
+        files: [ 'css/*.scss' ],
+        tasks: [ 'sass', 'autoprefixer', 'cssmin' ],
+        options: {
+          spawn: false,
+        },
+        /*livereload: {
+          livereload: true,
+          files: [ 'css/global.min.css' ],
+        }*/
+      },
+
     },
 
     // Normal tasks.
@@ -33,7 +59,7 @@ module.exports = function(grunt) {
     sass: {
       dist: {
         options: {
-          style: "compressed"
+          style: "extended"
         },
         files: {
           "css/global.css": "css/main.scss"
@@ -43,11 +69,27 @@ module.exports = function(grunt) {
 
     autoprefixer: {
       options: {
-        browsers: ['last 2 versions']
+        browsers: [ 'last 2 versions' ]
       },
       dist: {
         files: {
           "css/global.css": "css/global.css"
+        }
+      }
+    },
+
+    cssmin: {
+      target: {
+        files: {
+          'css/global.min.css': [ 'css/global.css' ]
+        }
+      }
+    },
+
+    uglify: {
+      target: {
+        files: {
+          'js/global.min.js': [ 'js/global.js' ]
         }
       }
     }
@@ -62,9 +104,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Register tasks (default).
-  grunt.registerTask('default', [ 'concat', 'sass', 'autoprefixer' ]);
+  grunt.registerTask('default', [ 'concat', 'sass', 'autoprefixer', 'cssmin', 'uglify' ]);
 
   // Register tasks (dev purposes).
   grunt.registerTask('dev', ['connect', 'watch']);
